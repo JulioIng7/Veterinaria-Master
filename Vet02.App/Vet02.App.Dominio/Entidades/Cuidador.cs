e51cba9de20290d55fc49317a68e223baa4167e3
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-05 19:55:12 -0500

    Update Cuidador.cs

--- Vet02.App/Vet02.App.Dominio/Entidades/Cuidador.cs
+++ Vet02.App/Vet02.App.Dominio/Entidades/Cuidador.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 namespace Vet02.App.Dominio
 {
     public class Cuidador : Persona
     {
+        public DateTime FechaInscripcion{set; get;}
         public string Direccion{set; get;}
         public string NumeroTelefono{set; get;}
+        
+        public ICollection<Mascota> Mascotas {set; get;}
     }
 }
