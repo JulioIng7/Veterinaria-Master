a30c18dfa92f0aa4431db62eaa4c99bd0e733b96
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-05 20:36:55 -0500

    Update Veterinario.cs

--- Vet02.App/Vet02.App.Dominio/Entidades/Veterinario.cs
+++ Vet02.App/Vet02.App.Dominio/Entidades/Veterinario.cs
@@ -8,7 +8,7 @@ namespace Vet02.App.Dominio
         public string TarjetaProfesional{set; get;}
         public string Especializacion{set; get;}
         public string TelefonoContacto{set; get;}
-        
+        //Relacion con la entidad Cita
         public ICollection<Cita> Citas{set; get;}
     }
 }
