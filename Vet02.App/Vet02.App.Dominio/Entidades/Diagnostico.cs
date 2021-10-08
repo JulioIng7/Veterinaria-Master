a82a202b2f529cd2a494508e7cd75e761911963a
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 18:32:18 -0500

    Update Diagnostico.cs

--- Vet02.App/Vet02.App.Dominio/Entidades/Diagnostico.cs
+++ Vet02.App/Vet02.App.Dominio/Entidades/Diagnostico.cs
@@ -7,7 +7,6 @@ namespace Vet02.App.Dominio
         public int Id{set; get;}
         public string DiagnosticoMascota{set; get;}
         public string Recomendaciones{set; get;}
-        public int IdHistoriaMedica{set; get;}
         //Relacion con la entidad HistoriaMedica
         public HistoriaMedica HistoriaMedica{set; get;}
         public int HistoriaMedicaId{set;get;}
