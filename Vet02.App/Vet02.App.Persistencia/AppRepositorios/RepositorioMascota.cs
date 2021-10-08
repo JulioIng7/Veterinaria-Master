6ff813976ff6da460731b20bb90123ebc076dfc0
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 18:20:44 -0500

    Update RepositorioMascota.cs

--- Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioMascota.cs
+++ Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioMascota.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using VeterinaryG02.App.Dominio;
+using Vet02.App.Dominio;
 
-namespace VeterinaryG02.App.Persistencia
+namespace Vet02.App.Persistencia
 {
     public class RepositorioMascota : IRepositorioMascota
     {
@@ -12,43 +12,47 @@ namespace VeterinaryG02.App.Persistencia
         {
             this.appContext = appContextParam;
         }
+        //Obtener una coleccion de datos de tipo Mascota de la tabla Mascotas en la base de datos
         IEnumerable<Mascota> IRepositorioMascota.GetAllMascotas()
         {
-            return this.appContext.TblMascota;
+            return this.appContext.Mascotas;
         }
-        Mascota IRepositorioMascota.AddMascota(Mascota nuevaMascota)
+        //Adicionar registro a tabla Mascotas de un objeto mascota        
+        Mascota IRepositorioMascota.AddMascota(Mascota mascota)
         {
-            var mascotaAdicionada = this.appContext.TblMascota.Add(nuevaMascota);
+            var mascotaAdicionada = this.appContext.Mascotas.Add(mascota);
             this.appContext.SaveChanges();
             return mascotaAdicionada.Entity;
         }
-        Mascota IRepositorioMascota.UpdateMascota(Mascota actMascota)
+        //Actualizar un registro de la tabla Mascotas con los datos del objeto nuevaMascota        
+        Mascota IRepositorioMascota.UpdateMascota(Mascota nuevaMascota)
         {
-            var mascotaEncontrada = this.appContext.TblMascota.FirstOrDefault(m => m.Id == actMascota.Id);
+            var mascotaEncontrada = this.appContext.Mascotas.FirstOrDefault(m => m.Id == nuevaMascota.Id);
             if(mascotaEncontrada != null)
             {
-                //mascotaEncontrada.CuidadorId = actMascota.CuidadorId;
-                mascotaEncontrada.Nombre = actMascota.Nombre;
-                mascotaEncontrada.Edad = actMascota.Edad;
-                mascotaEncontrada.TipoAnimal = actMascota.TipoAnimal;
-                mascotaEncontrada.Raza = actMascota.Raza;
-                mascotaEncontrada.Genero = actMascota.Genero;
+                mascotaEncontrada.Nombre = nuevaMascota.Nombre;
+                mascotaEncontrada.Edad = nuevaMascota.Edad;
+                mascotaEncontrada.TipoAnimal = nuevaMascota.TipoAnimal;
+                mascotaEncontrada.Raza = nuevaMascota.Raza;
+                mascotaEncontrada.Genero = nuevaMascota.Genero;
                 this.appContext.SaveChanges();
             }
             return mascotaEncontrada;
         }
-        Mascota IRepositorioMascota.GetMascota(int idMascota)
+        //Obtener un objeto de la clase Mascota registrado en la base de datos segun su Id        
+        Mascota IRepositorioMascota.GetMascota(int mascotaId)
         {
-            return this.appContext.TblMascota.FirstOrDefault(m => m.Id == idMascota);
+            return this.appContext.Mascotas.FirstOrDefault(m => m.Id == mascotaId);
         }
-        void IRepositorioMascota.DeleteMascota(int idMascota)
+        //Borrar un registro de la tabla Mascotas en la base de datos
+        void IRepositorioMascota.DeleteMascota(int mascotaId)
         {
-            var mascotaEncontrada = this.appContext.TblMascota.FirstOrDefault(m => m.Id == idMascota);
+            var mascotaEncontrada = this.appContext.Mascotas.FirstOrDefault(m => m.Id == mascotaId);
             if(mascotaEncontrada == null)
             {
                 return;
             }
-            this.appContext.TblMascota.Remove(mascotaEncontrada);
+            this.appContext.Mascotas.Remove(mascotaEncontrada);
             this.appContext.SaveChanges();
         }
     }
