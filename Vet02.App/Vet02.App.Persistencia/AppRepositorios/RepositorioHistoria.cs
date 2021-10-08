d674a7da432de02accdca22f09b981347735c09a
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 18:37:23 -0500

    Update RepositorioHistoria.cs

--- Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioHistoria.cs
+++ Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioHistoria.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-using VeterinaryG02.App.Dominio;
+using Vet02.App.Dominio;
 
-namespace VeterinaryG02.App.Persistencia
+namespace Vet02.App.Persistencia
 {
     public class RepositorioHistoria : IRepositorioHistoria
     {
@@ -12,42 +12,45 @@ namespace VeterinaryG02.App.Persistencia
         {
             this.appContext = appContextParam;
         }
+        //Obtener una coleccion de datos de tipo HistoriaMedica de la tabla Historias en la base de datos        
         IEnumerable<HistoriaClinica> IRepositorioHistoria.GetAllHistorias()
         {
-            return this.appContext.TblHistoriaClinica;
+            return this.appContext.Historias;
         }
-        HistoriaClinica IRepositorioHistoria.AddHistoria(HistoriaClinica nuevaHistoria)
+        //Adicionar registro a tabla Historias de un objeto historia      
+        HistoriaClinica IRepositorioHistoria.AddHistoria(HistoriaMedica historia)
         {
-            var historiaAdicionada = this.appContext.TblHistoriaClinica.Add(nuevaHistoria);
+            var historiaAdicionada = this.appContext.Historias.Add(historia);
             this.appContext.SaveChanges();
             return historiaAdicionada.Entity;
         }
-        HistoriaClinica IRepositorioHistoria.UpdateHistoria(HistoriaClinica actHistoria)
+        //Actualizar un registro de la tabla Historias con los datos del objeto nuevaHistoria        
+        HistoriaClinica IRepositorioHistoria.UpdateHistoria(HistoriaMedica nuevaHistoria)
         {
-            var historiaEncontrada = this.appContext.TblHistoriaClinica.FirstOrDefault(hc => hc.Id == actHistoria.Id);
+            var historiaEncontrada = this.appContext.Historias.FirstOrDefault(hc => hc.Id == nuevaHistoria.Id);
             if(historiaEncontrada != null)
             {
-                historiaEncontrada.Fecha = actHistoria.Fecha;
-                //historiaEncontrada.MascotaId = actHistoria.MascotaId;
-                historiaEncontrada.EstadoMascota = actHistoria.EstadoMascota;
-                historiaEncontrada.Observaciones = actHistoria.Observaciones;
-
+                historiaEncontrada.EstadoMascota = nuevaHistoria.EstadoMascota;
+                historiaEncontrada.Observaciones = nuevaHistoria.Observaciones;
+                historiaEncontrada.MascotaId = nuevaHistoria.MascotaId;
                 this.appContext.SaveChanges();
             }
             return historiaEncontrada;
         }
-        HistoriaClinica IRepositorioHistoria.GetHistoriaClinica(int idHistoria)
+        //Obtener un objeto de la clase HistoriaMedica registrado en la base de datos segun su Id        
+        HistoriaClinica IRepositorioHistoria.GetHistoriaMedica(int historiaId)
         {
-            return this.appContext.TblHistoriaClinica.FirstOrDefault(hc => hc.Id == idHistoria);
+            return this.appContext.Historias.FirstOrDefault(hc => hc.Id == historiaId);
         }
-        void IRepositorioHistoria.DeleteHistoria(int idHistoria)
+        //Borrar un registro de la tabla Historias en la base de datos        
+        void IRepositorioHistoria.DeleteHistoria(int historiaId)
         {
-            var historiaEncontrada = this.appContext.TblHistoriaClinica.FirstOrDefault(hc => hc.Id == idHistoria);
+            var historiaEncontrada = this.appContext.Historias.FirstOrDefault(hc => hc.Id == historiaId);
             if(historiaEncontrada == null)
             {
                 return;
             }
-            this.appContext.TblHistoriaClinica.Remove(historiaEncontrada);
+            this.appContext.Historias.Remove(historiaEncontrada);
             this.appContext.SaveChanges();
         }
     }
