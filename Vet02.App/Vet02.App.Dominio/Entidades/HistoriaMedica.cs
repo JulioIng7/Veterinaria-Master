d12996e553f239e987083a046f4571f249d7b125
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 18:24:48 -0500

    Update HistoriaMedica.cs

--- Vet02.App/Vet02.App.Dominio/Entidades/HistoriaMedica.cs
+++ Vet02.App/Vet02.App.Dominio/Entidades/HistoriaMedica.cs
@@ -7,7 +7,7 @@ namespace Vet02.App.Dominio
         public int Id{set; get;}
         public DateTime FechaHClinica{set; get;}
         public string EstadoMascota{set; get;}
-        public string Observaviones{set; get;}
+        public string Observaciones{set; get;}
         //Relacion con la entidad Diagnostico
         public Diagnostico Diagnostico{set; get;}
         //Relacion con la entidad Mascota
