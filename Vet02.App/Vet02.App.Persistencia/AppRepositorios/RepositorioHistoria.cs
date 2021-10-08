e855532ff320277c57fc905002dfd18f0356bb85
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 18:39:58 -0500

    Update RepositorioHistoria.cs

--- Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioHistoria.cs
+++ Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioHistoria.cs
@@ -13,19 +13,19 @@ namespace Vet02.App.Persistencia
             this.appContext = appContextParam;
         }
         //Obtener una coleccion de datos de tipo HistoriaMedica de la tabla Historias en la base de datos        
-        IEnumerable<HistoriaClinica> IRepositorioHistoria.GetAllHistorias()
+        IEnumerable<HistoriaMedica> IRepositorioHistoria.GetAllHistorias()
         {
             return this.appContext.Historias;
         }
         //Adicionar registro a tabla Historias de un objeto historia      
-        HistoriaClinica IRepositorioHistoria.AddHistoria(HistoriaMedica historia)
+        HistoriaMedica IRepositorioHistoria.AddHistoria(HistoriaMedica historia)
         {
             var historiaAdicionada = this.appContext.Historias.Add(historia);
             this.appContext.SaveChanges();
             return historiaAdicionada.Entity;
         }
         //Actualizar un registro de la tabla Historias con los datos del objeto nuevaHistoria        
-        HistoriaClinica IRepositorioHistoria.UpdateHistoria(HistoriaMedica nuevaHistoria)
+        HistoriaMedica IRepositorioHistoria.UpdateHistoria(HistoriaMedica nuevaHistoria)
         {
             var historiaEncontrada = this.appContext.Historias.FirstOrDefault(hc => hc.Id == nuevaHistoria.Id);
             if(historiaEncontrada != null)
@@ -38,7 +38,7 @@ namespace Vet02.App.Persistencia
             return historiaEncontrada;
         }
         //Obtener un objeto de la clase HistoriaMedica registrado en la base de datos segun su Id        
-        HistoriaClinica IRepositorioHistoria.GetHistoriaMedica(int historiaId)
+        HistoriaMedica IRepositorioHistoria.GetHistoriaMedica(int historiaId)
         {
             return this.appContext.Historias.FirstOrDefault(hc => hc.Id == historiaId);
         }
