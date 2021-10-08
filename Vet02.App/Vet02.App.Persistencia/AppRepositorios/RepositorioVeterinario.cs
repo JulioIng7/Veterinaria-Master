d08ade209cb58cc601330363b44c9b204cc90601
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 17:33:11 -0500

    Update RepositorioVeterinario.cs

--- Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioVeterinario.cs
+++ Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioVeterinario.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Vet02.App.Persistencia
 {
-    public class RepositorioVeterinario:IRepositorioVeterinario
+    public class RepositorioVeterinario : IRepositorioVeterinario
     {
         private readonly AppContext appContext;
 
@@ -14,38 +14,54 @@ namespace Vet02.App.Persistencia
         {
             this.appContext = appContextParam;
         }
-        
+        //Obtener una coleccion de datos de tipo Veterinario de la tabla Veterinarios de la base de datos
         IEnumerable<Veterinario> IRepositorioVeterinario.GetAllVeterinarios()
         {
-            return null;
-            //throw new System.NotImplementedException();
+            this.appContext.Veterinarios;
         }
+        //Adicionar registro a tabla Veterinarios de un objeto vet
         Veterinario IRepositorioVeterinario.AddVeterinario(Veterinario vet)
         {
             var veterinarioAdicionado = this.appContext.Veterinarios.Add(vet);
             this.appContext.SaveChanges();
             return veterinarioAdicionado.Entity;
-            //throw new System.NotImplementedException();
         }
-        Veterinario IRepositorioVeterinario.UpdateVeterinario(Veterinario vet)
+        //Actualizar un registro de la tabla Veterinarios con los datos del objeto nuevoVet
+        Veterinario IRepositorioVeterinario.UpdateVeterinario(Veterinario nuevoVet)
         {
-            return null;
-            //throw new System.NotImplementedException();
-        }
-        void IRepositorioVeterinario.DeleteVeterinario(int idVeterinario)
-        {
-            var veterinarioEncontrado = this.appContext.Veterinarios.FirstOrDefault(v => v.Id == idVeterinario);
+            var veterinarioEncontrado = this.appContext.Veterinarios.FirsOrDefault(v => v.Id == nuevoVet.Id);
             if(veterinarioEncontrado != null)
             {
-                this.appContext.Remove(veterinarioEncontrado);
+                veterinarioEncontrado.Nombre = nuevoVet.Nombre;
+                veterinarioEncontrado.Apellidos = nuevoVet.Apellidos;
+                veterinarioEncontrado.Edad = nuevoVet.Edad;
+                veterinarioEncontrado.NumeroDocumento = nuevoVet.NumeroDocumento;
+                veterinarioEncontrado.Sexo = nuevoVet.Sexo;
+                veterinarioEncontrado.Email = nuevoVet.Email;
+                veterinarioEncontrado.Password = nuevoVet.Password;
+                veterinarioEncontrado.TarjetaProfesional = nuevoVet.TarjetaProfesional;
+                veterinarioEncontrado.Especializacion = nuevoVet.Especializacion;
+                veterinarioEncontrado.TelefonoContacto = nuevoVet.TelefonoContacto;
                 this.appContext.SaveChanges();
             }
-            //throw new System.NotImplementedException();
+            
+            return veterinarioEncontrado;
         }
-        Veterinario IRepositorioVeterinario.GetVeterinario(int idVeterinario)
+        //Borrar un registro de la tabla Veterinarios en la base de datos
+        void IRepositorioVeterinario.DeleteVeterinario(int veterinarioId)
         {
-            return this.appContext.Veterinarios.FirstOrDefault(v => v.Id == idVeterinario);
-            //throw new System.NotImplementedException();
+            var veterinarioEncontrado = this.appContext.Veterinarios.FirstOrDefault(v => v.Id == veterinarioId);
+            if(veterinarioEncontrado == null)
+            {
+                return;
+            }
+            this.appContext.Remove(veterinarioEncontrado);
+            this.appContext.SaveChanges();
+        }
+        //Obtener un objeto de la clase Administrador registrado en la base de datos segun su Id
+        Veterinario IRepositorioVeterinario.GetVeterinario(int veterinarioId)
+        {
+            return this.appContext.Veterinarios.FirstOrDefault(v => v.Id == veterinarioId);
         }
     }
 }
