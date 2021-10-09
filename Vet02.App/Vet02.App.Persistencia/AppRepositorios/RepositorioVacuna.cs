dd66aba2eb59a8be628e00b605ebbfddb5d9ba75
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 19:48:53 -0500

    Update RepositorioVacuna.cs

--- Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioVacuna.cs
+++ Vet02.App/Vet02.App.Persistencia/AppRepositorios/RepositorioVacuna.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-using VeterinaryG02.App.Dominio;
+using Vet02.App.Dominio;
 
-namespace VeterinaryG02.App.Persistencia
+namespace Vet02.App.Persistencia
 {
     public class RepositorioVacuna : IRepositorioVacuna
     {
@@ -12,44 +12,49 @@ namespace VeterinaryG02.App.Persistencia
         {
             this.appContext = appContextParam;
         }
+        //Obtener una coleccion de datos de tipo Vacuna de la tabla Vacunas en la base de datos        
         IEnumerable<Vacuna> IRepositorioVacuna.GetAllVacunas()
         {
-            return this.appContext.TblVacuna;
+            return this.appContext.Vacunas;
         }
-        Vacuna IRepositorioVacuna.AddVacuna(Vacuna nuevaVacuna)
+        //Adicionar registro a tabla Vacunas de un objeto vacuna        
+        Vacuna IRepositorioVacuna.AddVacuna(Vacuna vacuna)
         {
-            var vacunaAdicionada = this.appContext.TblVacuna.Add(nuevaVacuna);
+            var vacunaAdicionada = this.appContext.Vacunas.Add(vacuna);
             this.appContext.SaveChanges();
             return vacunaAdicionada.Entity;
         }
-        Vacuna IRepositorioVacuna.UpdateVacuna(Vacuna actVacuna)
+        //Actualizar un registro de la tabla Vacunas con los datos del objeto nuevaVacuna        
+        Vacuna IRepositorioVacuna.UpdateVacuna(Vacuna nuevaVacuna)
         {
-            var vacunaEncontrada = this.appContext.TblVacuna.FirstOrDefault(v => v.Id == actVacuna.Id);
+            var vacunaEncontrada = this.appContext.TblVacuna.FirstOrDefault(v => v.Id == nuevaVacuna.Id);
             if(vacunaEncontrada != null)
             {
-                //vacunaEncontrada.MascotaId = actVacuna.MascotaId;
-                vacunaEncontrada.Laboratorio = actVacuna.Laboratorio;
-                vacunaEncontrada.Lote = actVacuna.Lote;
-                vacunaEncontrada.Serie = actVacuna.Serie;
-                vacunaEncontrada.FechaProduccion = actVacuna.FechaProduccion;
-                vacunaEncontrada.FechaVencimiento = actVacuna.FechaVencimiento;
-                vacunaEncontrada.FechaAplicacion = actVacuna.FechaAplicacion;
+                vacunaEncontrada.Laboratorio = nuevaVacuna.Laboratorio;
+                vacunaEncontrada.Lote = nuevaVacuna.Lote;
+                vacunaEncontrada.Serie = nuevaVacuna.Serie;
+                vacunaEncontrada.FechaProduccion = nuevaVacuna.FechaProduccion;
+                vacunaEncontrada.FechaVencimiento = nuevaVacuna.FechaVencimiento;
+                vacunaEncontrada.FechaAplicacion = nuevaVacuna.FechaAplicacion;
+                vacunaEncontrada.MascotaId = nuevaVacuna.MascotaId;
                 this.appContext.SaveChanges();
             }
             return vacunaEncontrada;
         }
-        Vacuna IRepositorioVacuna.GetVacuna(int idVacuna)
+        //Obtener un objeto de la clase Vacuna registrado en la base de datos segun su Id        
+        Vacuna IRepositorioVacuna.GetVacuna(int vacunaId)
         {
-            return this.appContext.TblVacuna.FirstOrDefault(v => v.Id == idVacuna);
+            return this.appContext.Vacunas.FirstOrDefault(v => v.Id == vacunaId);
         }
-        void IRepositorioVacuna.DeleteVacuna(int idVacuna)
+        //Borrar un registro de la tabla Vacunas en la base de datos        
+        void IRepositorioVacuna.DeleteVacuna(int vacunaId)
         {
-            var vacunaEncontrada = this.appContext.TblVacuna.FirstOrDefault(v => v.Id == idVacuna);
+            var vacunaEncontrada = this.appContext.Vacunas.FirstOrDefault(v => v.Id == vacunaId);
             if(vacunaEncontrada == null)
             {
                 return;
             }
-            this.appContext.TblVacuna.Remove(vacunaEncontrada);
+            this.appContext.Vacunas.Remove(vacunaEncontrada);
             this.appContext.SaveChanges();
         }
     }
