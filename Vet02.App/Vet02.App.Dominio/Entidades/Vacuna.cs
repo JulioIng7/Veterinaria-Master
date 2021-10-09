4e9d06a67081da9b89fb1b2f3320aeb4eccff0d0
RodMurJS44 <91105257+RodMurJS44@users.noreply.github.com>
2021-10-08 19:44:45 -0500

    Update Vacuna.cs

--- Vet02.App/Vet02.App.Dominio/Entidades/Vacuna.cs
+++ Vet02.App/Vet02.App.Dominio/Entidades/Vacuna.cs
@@ -10,7 +10,7 @@ namespace Vet02.App.Dominio
         public string Lote{set; get;}
         public DateTime FechaProduccion{set; get;}
         public DateTime FechaVencimiento{set; get;}
-        public DateTime FechaAplicacion{set; get;}
+        public DateTime FechaAplicacion{set; get;} = DateTime.Now;
         //Relacion con la entidad Mascota
         public Mascota Mascota{set; get;}
         public Mascota MascotaId{set; get;}
